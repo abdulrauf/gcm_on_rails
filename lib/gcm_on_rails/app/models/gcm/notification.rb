9c0e2f63b5aa345cb5aaeddd4f2ddf5bdd358f33
Abdul Rauf <abdul.rauf.au@gmail.com>
2012-08-24 11:53:30 +0930

    Update lib/gcm_on_rails/app/models/gcm/notification.rb

--- lib/gcm_on_rails/app/models/gcm/notification.rb
+++ lib/gcm_on_rails/app/models/gcm/notification.rb
@@ -52,18 +52,18 @@ class Gcm::Notification < Gcm::Base
                 when "MissingRegistration"
                   ex = Gcm::Errors::MissingRegistration.new(response[:message])
                   logger.warn("#{ex.message}, destroying gcm_device with id #{notification.device.id}")
-                  notification.device.destroy
+                  #notification.device.destroy
                 when "InvalidRegistration"
                   ex = Gcm::Errors::InvalidRegistration.new(response[:message])
                   logger.warn("#{ex.message}, destroying gcm_device with id #{notification.device.id}")
-                  notification.device.destroy
+                  #notification.device.destroy
                 when "MismatchedSenderId"
                   ex = Gcm::Errors::MismatchSenderId.new(response[:message])
                   logger.warn(ex.message)
                 when "NotRegistered"
                   ex = Gcm::Errors::NotRegistered.new(response[:message])
                   logger.warn("#{ex.message}, destroying gcm_device with id #{notification.device.id}")
-                  notification.device.destroy
+                  #notification.device.destroy
                 when "MessageTooBig"
                   ex = Gcm::Errors::MessageTooBig.new(response[:message])
                   logger.warn(ex.message)

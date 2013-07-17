0227cabcda03a7a84c855191591497f9f133aab1
Abdul Rauf <abdul.rauf.au@gmail.com>
2013-07-17 14:05:58 +0930

    Update create_gcm_devices.rb

--- lib/generators/templates/gcm_migrations/create_gcm_devices.rb
+++ lib/generators/templates/gcm_migrations/create_gcm_devices.rb
@@ -1,12 +1,14 @@
 class CreateGcmDevices < ActiveRecord::Migration # :nodoc:
   def self.up
     create_table :gcm_devices do |t|
+      t.string :device_id, :size => 120
       t.string :registration_id, :size => 120, :null => false
       t.datetime :last_registered_at
 
       t.timestamps
     end
 
+    add_index :gcm_devices, :device_id, :unique => true
     add_index :gcm_devices, :registration_id, :unique => true
   end
 
